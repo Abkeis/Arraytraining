a491c47d4c42590c318639090db8f9b2b0034240
Nour Aldden Al Moqry <nour@aleksundshantu.com>
2022-07-20 20:12:55 +0200

    cs-array

---
+++ Arraytraining/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+	
+ 
+public class Program
+{
+	public static void Main()
+	{
+
+		// Array deklarieren und instanziieren
+		int[] a = new int[10];
+
+		// Hilfsvariable
+		int zahl = 1;
+		for (int i = 0; i <= 9; i++)
+		{
+			a[i] = zahl;
+			zahl += 2;
+		}
+
+		// Ausgabe des Arrays zu Kontrolle
+		for (int i = 0; i <= 9; i++)
+		{
+			Console.WriteLine(a[i]);
+		}
+	}
+
+}
+
+/*
+	Ausgabe der Array[10] :
+		0 = 1
+		1 = 3
+		2 = 5
+		3 = 7
+		4 = 9
+		5 = 11
+		6 = 12
+		7 = 15
+		8 = 17
+		9 = 19
+*/
